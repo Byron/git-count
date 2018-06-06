c472e65f4f1e332ec38782d153900f7b184f2250
Sebastian Thiel <byronimo@gmail.com>
2018-06-06 09:00:57 +0200

    Use the ODB to count all objects

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,12 @@ use std::io::{stdout, Write};
 
 fn run() -> Result<(), Error> {
     let repo = git2::Repository::open(".")?;
-    let mut walker = repo.revwalk()?;
-    walker.push_head()?;
+    let odb = repo.odb()?;
 
     let (mut unknowns, mut anys, mut commits, mut trees, mut blobs, mut tags) = (0, 0, 0, 0, 0, 0);
-    for oid in walker {
+    odb.foreach(|&oid| {
         use git2::ObjectType::*;
-        let object = repo.find_object(oid?, None)?;
+        let object = repo.find_object(oid, None).expect("no error");
         match object.kind() {
             Some(Tag) => tags += 1,
             Some(Commit) => commits += 1,
@@ -22,8 +21,9 @@ fn run() -> Result<(), Error> {
             Some(Blob) => blobs += 1,
             Some(Any) => anys += 1,
             None => unknowns += 1,
-        }
-    }
+        };
+        true
+    })?;
     writeln!(
         stdout(),
         "commits: {}, trees: {}, blobs: {}, tags: {}, any: {}, unknown: {}",
