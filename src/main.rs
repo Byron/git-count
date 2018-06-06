ef149677f4e0c14b797c10084c78a113e3977260
Sebastian Thiel <sthiel@thoughtworks.com>
2018-06-06 13:31:15 +0200

    Add progress bar to get a better idea about performance

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 extern crate failure;
 extern crate failure_tools;
 extern crate git2;
+extern crate indicatif;
 
 use failure::{Error, ResultExt};
 use failure_tools::ok_or_exit;
 use std::io::{stdout, Write};
 
+const PROGRESS_RESOLUTION: usize = 250;
+
 fn run() -> Result<(), Error> {
     let repo = git2::Repository::open(".")?;
     let odb = repo.odb()?;
+    let progress = indicatif::ProgressBar::new_spinner();
+    progress.set_draw_target(indicatif::ProgressDrawTarget::stderr());
 
-    let (mut unknowns, mut anys, mut commits, mut trees, mut blobs, mut tags) = (0, 0, 0, 0, 0, 0);
+    let (mut total, mut unknowns, mut anys, mut commits, mut trees, mut blobs, mut tags) = (0, 0, 0, 0, 0, 0, 0);
     odb.foreach(|&oid| {
         use git2::ObjectType::*;
         let object = repo.find_object(oid, None).expect("no error");
@@ -22,11 +27,20 @@ fn run() -> Result<(), Error> {
             Some(Any) => anys += 1,
             None => unknowns += 1,
         };
+        total += 1;
+        if total % PROGRESS_RESOLUTION == 0 {
+            progress.set_message(&format!("Counted {} objects...", total));
+            progress.tick();
+        }
         true
     })?;
+    if total / PROGRESS_RESOLUTION > 0 {
+        progress.finish_and_clear();
+    }
     writeln!(
         stdout(),
-        "commits: {}, trees: {}, blobs: {}, tags: {}, any: {}, unknown: {}",
+        "total: {}, commits: {}, trees: {}, blobs: {}, tags: {}, any: {}, unknown: {}",
+        total,
         commits,
         trees,
         blobs,
